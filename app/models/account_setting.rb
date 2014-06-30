3edc4ab2fc30402354ed6a06413a5e29d652775e
xing <xingxing2d@gmail.com>
2014-06-30 14:09:43 +0800

    blog_alias validation

--- app/models/account_setting.rb
+++ app/models/account_setting.rb
@@ -6,6 +6,9 @@ class AccountSetting < ActiveRecord::Base
   validates :account_id, uniqueness: true
   validates :blog_alias, uniqueness: true, length: { minimum: 3 }
   validate :validate_block_list
+  validates_format_of     :blog_alias,
+                          :with => /\A[a-zA-Z0-9\-]*?\z/,
+                          :message => 'accepts only letters, numbers'
 
   def validate_block_list
     errors.add(:blog_alias, 'is not allowed.') if BLOCK_LIST.include?(self.blog_alias.to_s.downcase)
