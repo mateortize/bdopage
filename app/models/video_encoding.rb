bf9e8588785f85e13511ed92dae285ceb091d52e
xing <xingxing2d@gmail.com>
2014-07-04 17:15:46 +0800

    quick issue

--- app/models/video_encoding.rb
+++ app/models/video_encoding.rb
@@ -1,7 +1,7 @@
 class VideoEncoding < ActiveRecord::Base
   belongs_to :video
 
-  validates :profile_name, presence: true, scope: [:video_id]
+  validates :profile_name, presence: true
 
   def self.create_from_panda_encoding(encoding)
     self.create(
