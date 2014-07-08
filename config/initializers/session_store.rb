a74d33d6821700dd4df029ae64a78b255a6f6912
xing <xingxing2d@gmail.com>
2014-07-08 15:18:25 +0800

    share session for all subdomains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, key: '_videopage7_session', domain: 'videopage7.com'
+else
+  Rails.application.config.session_store :cookie_store, key: '_videopage7_session'
+end
 
-Rails.application.config.session_store :cookie_store, key: '_videopage7_session'
