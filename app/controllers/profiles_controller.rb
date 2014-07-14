e16b2d48575c68ccc06a7a4307230c01d0835dfe
xing <xingxing2d@gmail.com>
2014-07-15 01:59:55 +0800

    fixed following

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,8 @@
 class ProfilesController < ApplicationController
   skip_before_action :authenticate_account!
   
-
+  layout 'bgwhite'
+  
   def show
     @account = @current_author
     @profile = @current_author.profile
