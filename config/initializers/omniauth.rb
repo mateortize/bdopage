9cb37123c9abb9e5d91aa5004a24ef4ba42d2d5e
xing <xingxing2d@gmail.com>
2014-07-17 17:23:30 +0800

    bonofa omniauth update

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,9 @@
-if Rails.env.development?
-  provider :bonofa, bonofa_app_id, bonofa_app_secret, client_options: {
-    site: "http://baio.sevendevs.de"
-  }
-end
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :bonofa, Rails.application.secrets[:bonofa]["app_id"], Rails.application.secrets[:bonofa]["secret_key"]
+  if Rails.env.development?
+    provider :bonofa, Rails.application.secrets[:bonofa]["app_id"], Rails.application.secrets[:bonofa]["secret_key"], client_options: {
+      site: "http://baio.sevendevs.de"
+    }
+  else
+    provider :bonofa, Rails.application.secrets[:bonofa]["app_id"], Rails.application.secrets[:bonofa]["secret_key"]
+  end
 end
