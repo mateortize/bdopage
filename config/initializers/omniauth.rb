152ceaec8ba9a4a94f22fcfda26f6402edd56a9d
Ubuntu <ubuntu@ip-172-31-19-119.eu-west-1.compute.internal>
2014-08-06 07:05:07 +0000

    merged

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,14 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-<<<<<<< HEAD
-    if Rails.env.development?
-      provider :bonofa, Rails.application.secrets[:bonofa]["app_id"], Rails.application.secrets[:bonofa]["secret_key"], client_options: {
-        site: "http://baio.sevendevs.de"
-      }
-    else
-      provider :bonofa, Rails.application.secrets[:bonofa]["app_id"], Rails.application.secrets[:bonofa]["secret_key"]
-    end
-end
-=======
   if Rails.env.development?
     provider :bonofa, Rails.application.secrets[:bonofa]["app_id"], Rails.application.secrets[:bonofa]["secret_key"], client_options: {
       site: "http://baio.sevendevs.de"
@@ -17,4 +7,3 @@ end
     provider :bonofa, Rails.application.secrets[:bonofa]["app_id"], Rails.application.secrets[:bonofa]["secret_key"]
   end
 end
->>>>>>> bbcf953fbf98db33bcf6ebf37dbd20d0c2368557
