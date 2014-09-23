27ac5361088d9867fb0afd695095e3157d088092
drosa88 <mail@danielederosa.de>
2014-09-23 16:49:10 +0200

    login images

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -2,18 +2,6 @@ function profiletoggle() {
     $( "#profiletoggle" ).click(function(event) {
         event.preventDefault();
         $( "#profile" ).slideToggle( "slow" );
-
-        var icon = $('#profiletoggle .toggle-icon');
-        var title = $('#profiletoggle .toggle-title');
-
-        if(icon.hasClass('glyphicon-circle-arrow-down')) {
-            title.replaceWith('<span class="toggle-title">Close profile</span>');
-            icon.removeClass('glyphicon-circle-arrow-down').addClass('glyphicon-circle-arrow-up');
-        }
-        else {
-            title.replaceWith('<span class="toggle-title">Open profile</span>');
-            icon.removeClass('glyphicon-circle-arrow-up').addClass('glyphicon-circle-arrow-down');
-        }
     });
 }
 
