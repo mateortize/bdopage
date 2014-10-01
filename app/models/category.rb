eb1c3f430e72d01b0253438fa31e8f8cfba52272
xing <xingxing2d@gmail.com>
2014-10-02 01:01:20 +0800

    sidebar category changes

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,10 @@ class Category < ActiveRecord::Base
   has_many :posts, dependent: :nullify
   validates_uniqueness_of :name
 
+  def posts_for(account_id)
+    self.posts.where(account_id: account_id)
+  end
+
   def self.options_for_select
     Category.all.collect do |category|
       [category.id, category.name]      
